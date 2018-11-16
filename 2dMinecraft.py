9d8d0127ddec6059871d3adbab36094d71352063
Oscar Dodd <oscardodd04@gmail.com>
2018-11-16 09:30:21 +0000

    Add files via upload

--- 2dMinecraft.py
+++ 2dMinecraft.py
@@ -12,7 +12,7 @@ BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 
 #Zombie constants
-ZOMBIE_DIST = 10
+ZOMBIE_DIST = 3
 zombieNotDead = True
 #Cloud position variable
 cloudx = -200
@@ -28,10 +28,15 @@ STONE = 6
 DIAMOND = 7
 CLOUD = 8
 ZOMBIE = 9
+HEART = 10
+FOOD = 11
 
 #Setting up the player
 PLAYER = pygame.image.load('sprites/player.png')
 playerPos = [0, 0]
+playerHealth = 5
+playerFood = 10
+amountOfFood = 0
 #Colour to resource dictionary
 textures = {
             DIRT:  pygame.image.load('sprites/dirt.png'),
@@ -43,7 +48,9 @@ textures = {
             STONE: pygame.image.load('sprites/stone.png'),
             DIAMOND: pygame.image.load('sprites/diamond.png'),
             CLOUD: pygame.image.load('sprites/cloud.png'),
-            ZOMBIE:pygame.image.load('sprites/zombie.png')
+            ZOMBIE:pygame.image.load('sprites/zombie.png'),
+            HEART:pygame.image.load('sprites/heart.png'),
+            FOOD:pygame.image.load('sprites/food.png')
 }
 
 #Defining the inventory
@@ -67,7 +74,8 @@ controls = {
             LAVA : 53,
             WOOD: 54,
             STONE: 55,
-            DIAMOND: 56
+            DIAMOND: 56,
+            FOOD: 57
 }
 #Crafting rules
 craft = {
@@ -75,12 +83,14 @@ craft = {
         LAVA  : {WATER: 2, COAL : 2},
         DIAMOND : {DIRT: 100},
         COAL : {STONE : 2, LAVA: 2},
-        WOOD: {LAVA : 2, STONE:2}
+        WOOD: {LAVA : 2, STONE:2},
+        FOOD: {GRASS : 5, DIRT: 5}
 }
 #Game Dims
+#DEFAULT TILE SIZE = 40
 TITLESIZE = 40
-MAPWIDTH = 30
-MAPHEIGHT = 20
+MAPWIDTH = 24
+MAPHEIGHT = 18
 
 #Tilemap
 resources = [DIRT, GRASS, WATER, COAL, WOOD, LAVA, STONE, DIAMOND]
@@ -92,7 +102,7 @@ for rw in range(MAPHEIGHT):
 
         if randomNumber >= 23 and randomNumber <= 25:
             tile = COAL
-        elif randomNumber == 1:
+        elif randomNumber >= 25 and randomNumber <= 27:
             tile = LAVA
         elif randomNumber >=3 and randomNumber <= 7:
             tile = DIRT
@@ -108,7 +118,7 @@ for rw in range(MAPHEIGHT):
             tile = GRASS
         tilemap[rw][cl] = tile
 
-DISPLAYSURF = pygame.display.set_mode((MAPWIDTH * TITLESIZE, MAPHEIGHT*TITLESIZE + 50))
+DISPLAYSURF = pygame.display.set_mode((MAPWIDTH * TITLESIZE, MAPHEIGHT*TITLESIZE + 100))
 pygame.display.set_caption('2d Minecraft')
 fonts = pygame.font.get_fonts()
 
@@ -158,7 +168,7 @@ while True:
         for column in range(MAPWIDTH):
             DISPLAYSURF.blit(textures[tilemap[row][column]], (column*TITLESIZE, row*TITLESIZE))
     DISPLAYSURF.blit(PLAYER, (playerPos[0] * TITLESIZE, playerPos[1] * TITLESIZE))
-
+    hpos = 0
     placePosition = 10
     for item in resources:
         DISPLAYSURF.blit(textures[item], (placePosition, MAPHEIGHT * TITLESIZE + 20))
@@ -166,7 +176,21 @@ while True:
         textObj = INVFONT.render(str(inventory[item]), True, WHITE, BLACK)
         DISPLAYSURF.blit(textObj, (placePosition, MAPHEIGHT * TITLESIZE + 20))
         placePosition += 50
+        hpos = placePosition
+    DISPLAYSURF.blit(textures[FOOD], (hpos, MAPHEIGHT * TITLESIZE + 20))
+    textObj = INVFONT.render(str(amountOfFood), True, WHITE, BLACK)
+    DISPLAYSURF.blit(textObj, (hpos, MAPHEIGHT * TITLESIZE + 20))
+    hpos += 50
+    fpos = hpos
+    for i in range(0, playerFood):
+        DISPLAYSURF.blit(textures[FOOD], (fpos, MAPHEIGHT * TITLESIZE+ 40))
+        fpos += 20
+    for i in range(0, playerHealth):
+        DISPLAYSURF.blit(textures[HEART], (hpos, MAPHEIGHT * TITLESIZE+ 20))
+        hpos += 20
+                    
     DISPLAYSURF.blit(textures[CLOUD].convert_alpha(), (cloudx, cloudy))
+
     cloudx += 1
     if cloudx > MAPWIDTH * TITLESIZE:
         cloudy = random.randint(0, MAPHEIGHT * TITLESIZE)
@@ -175,20 +199,33 @@ while True:
     cloudy2 = cloudy - 200
     zombiePos = [playerPos[0] - ZOMBIE_DIST, playerPos[1] - ZOMBIE_DIST]
     DISPLAYSURF.blit(textures[CLOUD].convert_alpha(), (cloudx2, cloudy2))
-
     DISPLAYSURF.blit(textures[ZOMBIE], ((playerPos[0] - ZOMBIE_DIST) * TITLESIZE, (playerPos[1] - ZOMBIE_DIST) * TITLESIZE))
-
-    zombieBlock = tilemap[zombiePos[0]][zombiePos[1]]
-    if resources[zombieBlock] == LAVA:
-        print(resources[zombieBlock])
-        print("LAVA")
-        ZOMBIE_DIST = 10
+    try:
+        zombieBlock = tilemap[zombiePos[0]][zombiePos[1]]
+    except:
+        print("ERROR")
+    #print(resources[zombieBlock])
+    #print(zombieBlock)
+    currentTile = tilemap[playerPos[1]][playerPos[0]]
+    print(currentTile)
+    if zombieBlock == 4:
+        #print(resources[zombieBlock])
+        #print("LAVA")
+        ZOMBIE_DIST = 5
+        zombieNotDead = False
+        
     if zombiePos[0] == playerPos[0] and zombiePos[1] == playerPos[1]:
-        print("DEAD")
+        ZOMBIE_DIST = 5
+        playerHealth -= 1
+        
+    if playerHealth == 0 or playerFood == 0:
         pygame.quit()
         sys.exit()
     chance = random.randint(0, 100)
     if chance == 5:
         ZOMBIE_DIST -= 1
+    hungerLoss = random.randint(1, 100)
+    if hungerLoss == 100:
+        playerFood -= 1
     pygame.display.update()
     fpsClock.tick(24)
